9d053d93c62e9c054a243e5f6e544ae50ab522b6
Pedro Longue <pedrolongue13@gmail.com>
2021-07-13 20:18:29 -0300

    Adiciona o State

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AluraCommons';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AluraCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 function ProfileSidebar (props) {
@@ -10,17 +11,33 @@ function ProfileSidebar (props) {
         src={`https://github.com/${props.githubUser}.png`}
         style={{ borderRadius: "8px" }}
       />
+      <hr />
+      <p>
+        <a className="boxLink" href={`https://github.com/${props.githubUser}`}>
+          @{props.githubUser}
+        </a>
+      </p>
+      <hr />
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   );
 }
 
 export default function Home() {
   const User = "PedroLongue";
+  const [comunity, setComunity] = React.useState([{
+    id: '213615616165416419646196415184',
+    title: 'Eu odeio acordar cedo',
+    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+  }
+  ]);
+
   const pessoasFavoritas = [
     'thiagobs1',
     'jessicadinizz', 
     'leticiabussinger'
   ]
+
   return (
     <div>
       <AlurakutMenu />
@@ -35,6 +52,40 @@ export default function Home() {
             </h1>
             <OrkutNostalgicIconSet />
           </Box>
+
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+            <form onSubmit={function handleCreateComunity(e) {
+              e.preventDefault();
+              const formData = new FormData(e.target);
+              const Newcomunity = {
+                id: new Date(),
+                titulo: formData.get('title'),
+                image: formData.get('image'),
+              }
+              const attComunity = [...comunity, Newcomunity];
+              setComunity(attComunity);
+            }}>
+              <div>
+                <input 
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  name="title"
+                  area-label="Qual vai ser o nome da sua comunidade?"
+                  type="text"
+                />
+              </div>
+              <div>
+                <input 
+                  placeholder="Coloque uma URL para usarmos de capa"
+                  name="image"
+                  area-label="Coloque uma URL para usarmos de capa"
+                />
+              </div>
+              <button>
+                Criar uma comunidade
+              </button>
+            </form>
+          </Box>
         </div>
         <div
           className="profileRelationsArea"
@@ -42,13 +93,30 @@ export default function Home() {
         >
           <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">
+              Comunidades ({comunity.length})
+            </h2>
+            <ul>
+              {comunity.map((itemAtual) => {
+                return (
+                  <li key={itemAtual.id}>
+                    <a href={`/users/${itemAtual.title}`}>
+                      <img src={itemAtual.image} />
+                      <span>{itemAtual.title}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
               Pessoas da comunidade ({pessoasFavoritas.length})
             </h2>
             <ul>
               {pessoasFavoritas.map((itemAtual) => {
                 return (
-                  <li>
-                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                  <li key={itemAtual}>
+                    <a href={`/users/${itemAtual}`}>
                       <img src={`https://github.com/${itemAtual}.png`} />
                       <span>{itemAtual}</span>
                     </a>
